66289e9d2302f1e6523c3e1db9d98ed1c3b9edc7
znarfen <znarfstrom@gmail.com>
2025-04-23 19:08:56 +0200

    other comit

--- src/Content.jsx
+++ src/Content.jsx
@@ -27,17 +27,8 @@ function Content() {
 
         return activeGenre;
     }
-    
-    function handleChange(key) {
-        setGenre((prev) => ({ ...prev, [key]: !prev[key] }));
-    }
-    
-    function onSubmit(event) {
-        event.preventDefault();
-    }
 
-    
-    useEffect(() => {
+    function getGames() {
         let gameList = [];
         fetch("/api/games?")
             .then(response => response.json())
@@ -53,7 +44,15 @@ function Content() {
                 setGames(gameList);
             })
             .catch(error => console.error(error));
-    })
+    }
+    
+    function handleChange(key) {
+        setGenre((prev) => ({ ...prev, [key]: !prev[key] }));
+    }
+    
+    useEffect(() => {
+        getGames();
+    }, [genres])
 
     // generate a "randome" value from int that is > max.
     function lessThan(int, max) {
@@ -68,16 +67,16 @@ function Content() {
         else return lessThan(Math.round(int)/2, max)
     }
 
-    // generate a "randome" rgb value from str, dark is max value for r, g and b.
-    function strToRGB(str, dark = 255) {
+    // generate a "randome" rgb value from str
+    function strToRGB(str) {
         let seed = 150;
         for (let pos = 0; pos < str.length; pos++) {
             seed += str.charCodeAt(pos) + pos
         }
 
-        let r = lessThan(seed, dark);
-        let g = lessThan(seed/5, dark);
-        let b = lessThan(seed/3, dark);
+        let r = lessThan(seed, 255);
+        let g = lessThan(seed/5, 255);
+        let b = lessThan(seed/3, 255);
 
         if (r < g && r < b) r = 0;
         else if (g < r && g < b) g = 0;
@@ -89,18 +88,21 @@ function Content() {
     return(
         <>
             <div className='sidebar'>
+                <h3>Game Genres</h3>
                 <form>
                     {Object.entries(genres).map(([key, value]) => (
-                        <p className='genre'>
-                            <label key={key}>
-                                {key}
-                                <input
-                                    checked={genres[key]}
-                                    onChange={() => handleChange(key)}
-                                    type="checkbox"
-                                />
-                            </label>
-                        </p>
+                        <>
+                            <p className='genre'>
+                                <label key={key}>
+                                    {key}
+                                    <input
+                                        checked={genres[key]}
+                                        onChange={() => handleChange(key)}
+                                        type="checkbox"
+                                    />
+                                </label>
+                            </p>
+                        </>
                     ))}
                 </form>
             </div>
@@ -112,7 +114,7 @@ function Content() {
                         <img src={game['thumbnail']}></img>
                         <h3>{game['title']}</h3>
                         <p className='description'>{game['short_description']}</p>
-                        <p className='genre' style={{backgroundColor: strToRGB(game['genre'], 255)}}>{game['genre']}</p>
+                        <p className='genre' style={{backgroundColor: strToRGB(game['genre'])}}>{game['genre']}</p>
                         <p className='platform'>{game['platform']}</p>
                     </div>
                 ))}
